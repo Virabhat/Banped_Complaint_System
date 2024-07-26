fa9774283415bc5bcb09f0f9cb45a068c178bb96
Virabhat <virabhat.kh@gmail.com>
2024-07-27 02:06:12 +0700

    เริ่มต้นสร้าง วันที่ 1

---
+++ resources/views/HomePage.blade.php
@@ -0,0 +1,12 @@
+@extends('Layouts.PageLayout')
+
+@section('CssPage')
+    <link rel="stylesheet" href="{{ asset('Css_Layouts/Page.css') }}">
+@endsection
+
+@section('title','ระบบร้องเรียนปัญหาในชุมชน:บ้านเป็ด')
+
+@section('content')
+    <h1>asdasd</h1>
+    <p>asdasdasdasd</p>
+@endsection
